ec79ba3ec92c6a99ff96ab8065053ed58b1bfbe2
Mushhood <mushhoodyousafzai@gmail.com>
2022-12-15 05:53:04 +0500

    first commit

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twilio: {
+    accountSid: "AC8578538907541a29b3f01489e698c5a8",
+    apiKey: "SKfce2c001a61ea4a82b541f312790e09c",
+    apiSecret: "F0JJjLnTc5lMwQ0kadceDj4woNU6rPa5"
+  }
+};
